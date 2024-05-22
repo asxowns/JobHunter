8521fbba1e87440d05050e5b463b53e09b9a6a27
green <1000pgm@gmail.com>
2024-05-22 22:03:04 +0900

    기업게시판 리포지토리 쿼리문 작업중

--- src/main/java/com/green/jobhunter/repository/EnterCommu_replyRepository.java
+++ src/main/java/com/green/jobhunter/repository/EnterCommu_replyRepository.java
@@ -1,1 +1,16 @@
 package com.green.jobhunter.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.green.jobhunter.entity.EnterCommu_reply;
+import com.green.jobhunter.entity.EnterpriseCommunity;
+
+@Repository
+public interface EnterCommu_replyRepository extends JpaRepository<EnterCommu_reply, Long>{
+	
+	List<EnterCommu_reply> findByEccode(EnterpriseCommunity enterpriseCommunity);
+	
+}
